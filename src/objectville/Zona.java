1d87ed168803a01de63c7f194c2c798cec14cfd8
Elcan <raisengit@gmail.com>
2026-05-23 20:40:20 +0300

    Update Zona.java

--- src/objectville/Zona.java
+++ src/objectville/Zona.java
@@ -25,3 +25,47 @@ public abstract class Zone extends Cell {
 		return true;
 	}
 
+public void reset() {
+        this.electricity = 0;
+        this.water = 0;
+        this.internet = 0;
+        this.hasSecurity = false;
+        this.hasHealth = false;
+        this.hasEducation = false;
+        this.population = 0;
+        this.goods = 0;
+        this.lifestyle = 0;
+    }
+    
+    public void update() {
+        if (noUtilities()) {
+            this.level = 0;
+            this.output = 0;
+            this.demand = 1;
+            return;
+        }
+        
+        if (this.level < 3) {
+            if (canLevelUp(this.level + 1)) {
+                this.level = this.level + 1;
+            }
+        }
+        if (this.level > 0) {
+            if (canLevelUp(this.level) == false) {
+                this.level = this.level - 1;
+            }
+        }
+        
+        calcOutput();
+        
+        if (this.output < 1) {
+            this.demand = 1;
+        } else {
+            this.demand = this.output;
+        }
+    }
+    
+    public abstract boolean noUtilities();
+    public abstract boolean canLevelUp(int target);
+    public abstract void calcOutput();
+}
